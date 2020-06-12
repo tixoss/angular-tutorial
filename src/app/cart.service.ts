88f76bacfdda20c2bc78dad48848a3726aed4faa
tixoss <sorcen@mail.ru>
2020-06-12 18:20:56 +0300

    HttpClient & method getShippingPrices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product) {
     this.items.push(product);
@@ -19,6 +23,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json')
   }
 
 }
